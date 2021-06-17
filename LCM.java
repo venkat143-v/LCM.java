37d7eeccd0d2330a23931a89351fdaf623355774
venkat143-v <81428675+venkat143-v@users.noreply.github.com>
2021-06-17 16:59:14 +0530

    Add files via upload

---
+++ LCM.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class LCM
+{
+public static void main(String[] args)
+{
+Scanner obj = new Scanner(System.in);
+System.out.println("Enter 2 numbers");
+int a = obj.nextInt();//2
+int b = obj.nextInt();//3
+int gcd=1;
+for(int i=1; i<=a && i<=b; i++)
+{
+if(a%i==0 && b%i==0)
+gcd = i;//gcd=1
+}
+int lcm=(a*b)/gcd;
+System.out.println("LCM= " +lcm);
+}}
